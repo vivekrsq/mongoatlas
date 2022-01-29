6c129b4a37a4df8a929244f836c9d6178e583afc
vivek <vivekrsq@gmail.com>
2022-01-29 17:55:09 +0530

    post request using form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import logo from './profile-logo.jpg';
 
 function Form() {
     const [values, setValues] = useState({
@@ -19,20 +20,34 @@ function Form() {
     let onSubmit = (e)=>{
         e.preventDefault();
         console.log(values);
+        const response = postData();
+        async function postData(url = 'http://locahost:3001/api/user', data = values) {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            
+            body: JSON.stringify(data)
+          });
+          return response.json();
+        }
+        console.log(response);
     }
 
 
     return (
-        <div className="container w-50 p-3 my-4">
+        <div className="container w-25 p-3 my-4 border border-primary">
+            <img src={logo} class="rounded mx-auto d-block w-25" alt="profile-image"></img>
             <form>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" value={values.email} onChange={handleChange} id="email" aria-describedby="emailHelp"/>
+                    <input type="email" className="form-control border border-primary" value={values.email} onChange={handleChange} id="email" aria-describedby="emailHelp"/>
                         
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" value={values.password} onChange={handleChange} id="password"/>
+                    <input type="password" className="form-control border border-primary" value={values.password} onChange={handleChange} id="password"/>
                 </div>
                 
                 <button type="submit" className="btn btn-primary w-100" onClick={onSubmit}>Submit</button>
